175147951e02f1048862e84bf9cc87d3f860fca7
AniketDBhatkar <aniketbhatkar786@gmail.com>
2025-10-11 11:27:10 +0530

    controlled Components and conditional rendering

---
+++ my-app3/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LanguageGrid from './components/LanguageGrid.jsx'
+import AddLanguage from './components/AddLanguage.jsx'
+const App = () => {
+  return (
+    <>
+      <AddLanguage/>
+      <LanguageGrid />
+    </>
+  )
+}
+
+export default App
